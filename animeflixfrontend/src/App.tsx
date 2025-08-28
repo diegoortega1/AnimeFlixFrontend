ff769a97be087a7ad77e0081d186a58656f6007b
Diego <diegoortega1606@gmail.com>
2025-08-28 08:43:48 +0200

    feat: create Header component

--- animeflixfrontend/src/App.tsx
+++ animeflixfrontend/src/App.tsx
@@ -1,31 +1,8 @@
-import { Search } from "lucide-react";
-import { Button } from "./components/ui/button";
 import "./globals.css";
+import { Header } from "./Header";
 
 function App() {
-  return (
-    <div className="flex p-4 justify-between">
-      <h1
-        className={
-          "text-4xl md:text-4xl font-extrabold text-white tracking-tight drop-shadow-lg"
-        }
-      >
-        Animeflix
-      </h1>
-      <div className="flex items-center">
-        <Button className="text-white" variant="ghost">
-          Series
-        </Button>
-        <Button className="text-white" variant="ghost">
-          Peliculas
-        </Button>
-        <Button className="text-white" variant="ghost">
-          <Search />
-          Buscar
-        </Button>
-      </div>
-    </div>
-  );
+  return <Header />;
 }
 
 export default App;
---
+++ animeflixfrontend/src/Header.tsx
@@ -0,0 +1,28 @@
+import { Button } from "./components/ui/button";
+import { Search } from "lucide-react";
+
+export function Header() {
+  return (
+    <div className="flex p-4 justify-between">
+      <h1
+        className={
+          "text-4xl md:text-4xl font-extrabold text-white tracking-tight drop-shadow-lg"
+        }
+      >
+        Animeflix
+      </h1>
+      <div className="flex items-center">
+        <Button className="text-white" variant="ghost">
+          Series
+        </Button>
+        <Button className="text-white" variant="ghost">
+          Peliculas
+        </Button>
+        <Button className="text-white" variant="ghost">
+          <Search />
+          Buscar
+        </Button>
+      </div>
+    </div>
+  );
+}
