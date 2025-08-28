e5571eb5d139e9958d5a5ef409b579d3768b206e
Diego <diegoortega1606@gmail.com>
2025-08-28 08:50:59 +0200

    feat: add category titles to Animeflix homepage

--- animeflixfrontend/src/App.tsx
+++ animeflixfrontend/src/App.tsx
@@ -2,7 +2,33 @@ import "./globals.css";
 import { Header } from "./Header";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <div className="p-4">
+        <div>
+          <h4 className="scroll-m-20 text-xl font-semibold tracking-tight text-white">
+            Los mas vistos
+          </h4>
+        </div>
+        <div>
+          <h4 className="scroll-m-20 text-xl font-semibold tracking-tight text-white">
+            Novedades en Animeflix
+          </h4>
+        </div>
+        <div>
+          <h4 className="scroll-m-20 text-xl font-semibold tracking-tight text-white">
+            Recomendaciones
+          </h4>
+        </div>
+        <div>
+          <h4 className="scroll-m-20 text-xl font-semibold tracking-tight text-white">
+            Series de deportes
+          </h4>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
